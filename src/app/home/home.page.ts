e9c1614f3d2452ac6ba08c9f437f62d74ee19178
JooYoo <jooyoo@outlook.com>
2019-07-20 09:11:23 +0200

    added picker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currency = ''
 
-  constructor() {}
+  constructor(private pickerCtrl: PickerController) { }
+
+  async showBasicPiker() {
+    let opts: PickerOptions = {
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Done'
+        }
+
+      ],
+      columns: [
+        {
+          name: 'currency',
+          options: [
+            { text: 'CNY', value: 'A' },
+            { text: 'EUR', value: 'B' },
+            { text: 'USD', value: 'C' }
+          ]
+        }
+      ]
+    };
+
+    let picker = await this.pickerCtrl.create(opts);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('currency');
+      this.currency = col.options[col.selectedIndex].text;
+    });
+
+  }
+
 
 }
